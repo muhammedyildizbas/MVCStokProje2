ca4906a85a709cc60ee9929ffd7982d3f9af3eef
muhammedyildiz3465@gmail.com <63956100+muhammedyildizbas@users.noreply.github.com>
2021-06-05 20:05:04 +0300

    HttpGet and Http Post

--- Controllers/KategoriController.cs
+++ Controllers/KategoriController.cs
@@ -17,5 +17,19 @@ namespace MvcProje.Controllers
             var degerler = db.TBLKATEGORILERs.ToList();
             return View(degerler);
         }
+        [HttpGet] //sayfa yüklenirken yapması gerekenleri belirler
+        public ActionResult YeniKategori()
+        {
+            return View();
+        }
+
+        [HttpPost] // sayfada bir işlem yapıldığı zaman devreye girer
+        public ActionResult YeniKategori(TBLKATEGORILER p1)
+        {
+            db.TBLKATEGORILERs.Add(p1);
+            db.SaveChanges();
+            return View();
+
+        }
     }
 }
