d5126e6438f1f7ce722afd7701d23615c5225484
muhammedyildiz3465@gmail.com <63956100+muhammedyildizbas@users.noreply.github.com>
2021-06-05 13:20:41 +0300

    Initial commit

---
+++ Controllers/KategoriController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcProje.Models.Entity;
+
+namespace MvcProje.Controllers
+{
+    public class KategoriController : Controller
+    {
+        // GET: Kategori
+        MvcDbStokEntities db = new MvcDbStokEntities();
+
+        public ActionResult Index()
+        {
+            var degerler = db.TBLKATEGORILERs.ToList();
+
+
+
+
+            return View(degerler);
+        }
+    }
+}
