854242a73593d1dd3b94ced73830dda7d0dbe40b
muhammedyildiz3465@gmail.com <63956100+muhammedyildizbas@users.noreply.github.com>
2021-06-07 10:07:49 +0300

    Popup modal üzerinde Satış yapma işlemi

--- Controllers/SatisController.cs
+++ Controllers/SatisController.cs
@@ -3,15 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using MvcProje.Models.Entity;
 namespace MvcProje.Controllers
 {
     public class SatisController : Controller
     {
         // GET: Satis
+        MvcDbStokEntities db = new MvcDbStokEntities();
         public ActionResult Index()
         {
             return View();
         }
+        [HttpGet]
+        public ActionResult YeniSatis()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniSatis(TBLSATISLAR p)
+        {
+            db.TBLSATISLARs.Add(p);
+            db.SaveChanges();
+            return View("Index");
+        }
+
     }
 }
